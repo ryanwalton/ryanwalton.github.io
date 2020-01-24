834f954a9d3133485e8130e6a6b1ddfccc759503
ryanwalton <31267577+ryanwalton@users.noreply.github.com>
2020-01-24 15:03:00 -0800

    Battleship upload

---
+++ BattleShip.java
@@ -0,0 +1,20 @@
+/*
+Ryan Walton
+11/24/2018
+Final Project: BattleShip
+Create a one-player verison of BattleShip
+ */
+package battleship;
+
+public class BattleShip {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GameBoard battleship = new GameBoard();
+        battleship.setVisible(true);
+        
+    }
+    
+}
